fac414f5917ab350faff9d2b637dd4aa80bf2f14
Haochen Pan <32943456+haochenpan@users.noreply.github.com>
2020-05-17 18:05:40 -0400

    update setupCass

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 
-## install CCM2
+### install CCM2
 ```shell script
 sudo su
 sudo apt-get install -y git
 cd && git clone https://github.com/haochenpan/CCM2.git
 . ~/CCM2/sh/setup.sh bsr
+```
+
+### switch Cassandra version
+```shell script
+# comment out line 38, 39 setupCass.py 
+# if you dont want to remove the folder and download again
+python3 ~/CCM2/py/setupCass.py bsr
+
 ```
--- py/setupCass.py
+++ py/setupCass.py
@@ -15,11 +15,14 @@ usage:
         python3 setupCass.py <branch-name>
         e.g. python3 setupCass.py abd
 """
-if len(sys.argv) == 1 or len(sys.argv) > 2:
+if len(sys.argv) == 1 or len(sys.argv) > 3:
     print(usage)
     exit(0)
 
 branch = sys.argv[1]
+if branch == 'no-cass':
+    exit(0)
+
 for k, v in branch_dict.items():
     if branch in v:
         endpoint = f'https://github.com/{k}/cassandra.git'
@@ -33,20 +36,30 @@ else:
 print("endpoint:", endpoint)
 print("branch:", branch)
 
-cmds = f"""
-echo **********start**********
-# rm -rf ~/cassandra
-# cd && git clone {endpoint}
-cd ~/cassandra && git reset --hard && git checkout {branch}
-cp {"~/CCM2/res/build.xml"} .
-cp {"~/CCM2/res/build.properties.default"} .
-ant build
-git status
-cd
-echo ***********done**********
-\n
-\n
-"""
+if len(sys.argv) == 3 and sys.argv[2] == 'no-rm':
+    cmds = f"""
+    echo **********start**********
+    cd ~/cassandra && git reset --hard && git checkout {branch}
+    cp ~/CCM2/res/build.xml .
+    cp ~/CCM2/res/build.properties.default .
+    ant build
+    git status
+    cd
+    echo ***********done**********
+    """
+else:
+    cmds = f"""
+    echo **********start**********
+    rm -rf ~/cassandra
+    cd && git clone {endpoint}
+    cd ~/cassandra && git reset --hard && git checkout {branch}
+    cp ~/CCM2/res/build.xml .
+    cp ~/CCM2/res/build.properties.default .
+    ant build
+    git status
+    cd
+    echo ***********done**********
+    """
 
 process = subprocess.Popen(cmds, shell=True)
 process.communicate(input=b'\n')
